d6e46f1777cf490a505dcb41449aff2f78661fe9
krarpit <kumar.arpit77@yahoo.com>
2024-01-04 11:47:07 +0530

    extended the lambda calculus with booleans and conditionals

--- parsing.rkt
+++ parsing.rkt
@@ -10,15 +10,23 @@
 ;; - ,identifier
 ;; - { λ {,identifier} ,SFS }
 ;; - { ,SFS ,SFS }
+;; - true
+;; - false
+;; - { if ,SFS ,SFS ,SFS }
 ;; - "any other s-expression"
-;; where identifier is any symbol except lambda
+;; where identifier is any symbol except λ
 
 #;
 (define (fn-for-sfs sexp)
   (match sexp
     [`,i #:when (identifier? i) (... i)]
-    [`{ lambda ,i . sfs } (... i
-                               (fn-for-sfs sfs))]
+    [`true (...)]
+    [`false (...)]
+    [`{ if ,p ,c ,a } (... (fn-for-sfs p)
+                           (fn-for-sfs c)
+                           (fn-for-sfs a))]
+    [`{ λ { ,i } ,sfs } (... i
+                             (fn-for-sfs sfs))]
     [`{ ,sexp1 ,sexp2 } (... (fn-for-sfs sexp1)
                              (fn-for-sfs sexp2))]
     [otherwise (...)]))
@@ -28,7 +36,12 @@
 ;; EFFECT: signals an error if no representation possible
 (define (parse/simple sexp)
   (match sexp
+    [`true (bool #t)]
+    [`false (bool #f)]
     [`,i #:when (identifier? i) (var i)]
+    [`{ if ,p ,c ,a } (_if (parse/simple p)
+                           (parse/simple c)
+                           (parse/simple a))]
     [`{ λ {,i} ,sfs } (fun i
                            (parse/simple sfs))]
     [`{ ,sexp1 ,sexp2 } (app (parse/simple sexp1)
@@ -44,9 +57,18 @@
       (app (fun 'f (app (fun 'x (app (var 'f) (app (var 'x) (var 'x))))
                         (fun 'x (app (var 'f) (app (var 'x) (var 'x))))))
            (fun 'x (var 'x))))
+(test (parse/simple `true) (bool #t))
+(test (parse/simple `false) (bool #f))
+(test (parse/simple `{ if true
+                          {λ {x} x}
+                          {λ {F} {λ {x} (F x)}}})
+      (_if (bool #t) (fun 'x (var 'x)) (fun 'F (fun 'x (app (var 'F) (var 'x))))))
+(test (parse/simple `{ if ((λ {x} x) true)
+                          true
+                          false})
+      (_if (app (fun 'x (var 'x)) (bool #t)) (bool #t) (bool #f)))
 
 ;; the following example is expected to diverge
 #;
 (test/exn (interp/simple
            (parse/simple `((λ (f) ((λ (x) (f (x x))) (λ (x) (f (x x))))) (λ (x) x)))) "")
-      
--- simple.rkt
+++ simple.rkt
@@ -12,7 +12,10 @@
 ;; interp. variable name in Simple
 (define (identifier? s)
   (and (symbol? s)
-       (not (equal? s "λ"))))
+       (not (equal? s "λ"))
+       (not (equal? s "true"))
+       (not (equal? s "false"))
+       (not (equal? s "if"))))
 
 (define ID1 'F)
 (define ID2 'x)
@@ -21,6 +24,8 @@
 
 (define-type Simple
   [var (v identifier?)]
+  [bool (b boolean?)]
+  [_if (pred Simple?) (conseq Simple?) (altern Simple?)]  ;; if is a Racket keyword
   [fun (i identifier?) (body Simple?)]
   [app (ratr Simple?) (rand Simple?)])
 
@@ -29,25 +34,41 @@
 ;; <Simple> ::=
 ;; (IDENTIFIERS)
 ;;          <id>
+;; (CONDITIONALS)
+;;          <bool>
+;;        | {if <Simple> <Simple> <Simple>}
 ;; (FUNCTIONS
-;;        | {λ {<id>} <Simple>}  --> these are the only canonicals in Simple
+;;        | {λ {<id>} <Simple>}
 ;;        | {<Simple> <Simple>}
+;; where <bool> = true | false
+;;   and <id> is any identifier satisfying (identifier?)
 
 #;
 (define (fn-for-simple s)
   (type-case Simple s
     [var (v) (... v)]
+    [bool (b) (... b)]
+    [_if (p c a) (... (fn-for-simple p)
+                      (fn-for-simple c)
+                      (fn-for-simple a))]
     [fun (i body) (... i
                        (fn-for-simple body))]
     [app (rator rand) (... (fn-for-simple rator)
                            (fn-for-simple rand))]))
-    
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Typing
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; TODO!!!
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Interpretation
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define-type Value
+  [boolV (b boolean?)]
   [funV (param symbol?) (body Simple?)])
 ;; interp. Simple runtime value
 
@@ -64,6 +85,10 @@
           (define (closed-simple?--bounds s bounds)
             (type-case Simple s
               [var (v) (if (member v bounds) #t #f)]
+              [bool (b) #t]
+              [_if (p c a) (and (closed-simple?--bounds p bounds)
+                                (closed-simple?--bounds c bounds)
+                                (closed-simple?--bounds a bounds))]
               [fun (i body) (closed-simple?--bounds body (cons i bounds))]
               [app (rator rand) (and (closed-simple?--bounds rator bounds)
                                      (closed-simple?--bounds rand bounds))]))]
@@ -78,15 +103,25 @@
 (define (interp/simple s)
   (local [(define (interp/simple-env s env)
             (type-case Simple s
-              [var (v) (lookup-env env v)]  ;; guranteed to exist
-              [fun (i body) (funV i body)]   ;; already a value
+              [var (v) (lookup-env env v)]
+              [bool (b) (boolV b)]
+              [_if (p c a) (let ([pv (interp/simple p)])
+                             (type-case Value pv
+                               [funV (p body) (error 'interp/simple)]
+                               [boolV (b) (if b
+                                              (interp/simple c)
+                                              (interp/simple a))]))]
+              [fun (i body) (funV i body)]
               [app (rator rand) (let ([ratorv (interp/simple-env rator env)]
                                       [randv (interp/simple-env rand env)])
-                                  (interp/simple-env (funV-body ratorv)
-                                                     (extend-env
-                                                      env
-                                                      (funV-param ratorv)
-                                                      randv)))]))]
+                                  (type-case Value ratorv
+                                    [boolV (b) (error 'interp/simple)]
+                                    [funV (p body)
+                                          (interp/simple-env (funV-body ratorv)
+                                                             (extend-env
+                                                              env
+                                                              (funV-param ratorv)
+                                                              randv))]))]))]
     (begin
       (unless (closed-simple? s)
         (error 'interp/simple "not a valid Simple expression: ~a" s))
@@ -95,3 +130,12 @@
 (test (interp/simple (fun 'x (var 'x))) (funV 'x (var 'x)))
 (test (interp/simple (app (fun 'x (var 'x)) (fun 'x (var 'x))))
       (funV 'x (var 'x)))
+(test (interp/simple (bool #t)) (boolV #t))
+(test (interp/simple (bool #f)) (boolV #f))
+(test (interp/simple (_if (bool #t)
+                          (fun 'x (var 'x))
+                          (fun 'F (fun 'x (app (var 'F) (var 'x))))))
+      (funV 'x (var 'x)))
+(test (interp/simple (_if (app (fun 'x (var 'x)) (bool #t)) (bool #t) (bool #f)))
+      (boolV #t))
+(test/exn (interp/simple (app (bool #t) (bool #f))) "")
